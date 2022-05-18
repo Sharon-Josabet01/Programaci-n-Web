4700b7e7f24ec51fd90c0cc7d9c8f3205e6aeb22
Sharon-Josabet01 <104519618+Sharon-Josabet01@users.noreply.github.com>
2022-05-18 11:56:40 -0500

    Práctica Arrays Anidados 2

--- Práctica Arrays Anidados/script.js
+++ Práctica Arrays Anidados/script.js
@@ -7,6 +7,7 @@ var arrMulti = [
     // Sub 0  // Sub 1  // Sub 2
 ]; 
 */
+
 /* 
     Definimos nuestro array vacio
 */
@@ -20,16 +21,42 @@ var pokemones = [];
 var nombre = document.getElementById('nombre');
 var tipo = document.getElementById('tipo');
 var poder = document.getElementById('poder');
+var mensaje = document.getElementById('mensaje');
 
 var agregar = document.getElementById('agregar');
 agregar.addEventListener('click', agregarPokemon);
+
+var mostrar = document.getElementById('mostrar');
+mostrar.addEventListener('click', mostrarPokemon);
 
 /* 
     Definir la función que va agregar pokemones al array
 */
 
-function agregarPokemon(){
+function agregarPokemon() {
     /* Aquí va el código */
-  //  Así agregamos datos a un array con .push
-    pokemones.push( nombre.value, tipo.value, poder.value );
-}
+    //  Así agregamos datos a un array con .push
+    pokemones.push( [nombre.value, tipo.value, poder.value] );
+
+    var i = pokemones.length - 1;
+
+    //Imprimimos el mensaje en la etiqueta p is mensaje
+    mensaje.innerHTML = 'Se Agregó ' + pokemones[i][0];
+    }
+
+/* 
+    Definimos la función que va a mostrar todos los 
+*/
+
+function mostrarPokemon () {
+        //Código a ejecutar
+        
+        document.write(
+            '<ul>' +
+            '<li><img src="' + pokemones[0][0] + '.png" alt="Imagen Pikachu"></li>' +
+            '<li>Pokemon: ' + pokemones [0][0] + '</li>' +
+            '<li>Tipo: ' + pokemones [0][1] + '</li>' +
+            '<li>Poder: ' + pokemones [0][2] + '</li>' +
+            '</ul>'
+            );
+    }
