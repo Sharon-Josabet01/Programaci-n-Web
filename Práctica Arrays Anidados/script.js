eff81b6afd61eb3d12ef9837df43bfab08d5e698
Sharon-Josabet01 <104519618+Sharon-Josabet01@users.noreply.github.com>
2022-05-20 13:54:01 -0500

    Práctica Arrays Anidados 3

--- Práctica Arrays Anidados/script.js
+++ Práctica Arrays Anidados/script.js
@@ -34,29 +34,51 @@ mostrar.addEventListener('click', mostrarPokemon);
 */
 
 function agregarPokemon() {
-    /* Aquí va el código */
-    //  Así agregamos datos a un array con .push
+    /* Aquí va el código que ejecuta la función*/
+
+// Condición if
+    if (nombre.value === ''){
+      // Código que se ejecutará
+       mensaje.innerHTML = 'Hey! primero completa los datos y luego agrega un pokemón!';
+    } else {
+     // Ejecuta es si no se cumpla la condición 
+         //  Así agregamos datos a un array con .push
     pokemones.push( [nombre.value, tipo.value, poder.value] );
 
     var i = pokemones.length - 1;
 
     //Imprimimos el mensaje en la etiqueta p is mensaje
     mensaje.innerHTML = 'Se Agregó ' + pokemones[i][0];
-    }
+    };
+ }
 
 /* 
     Definimos la función que va a mostrar todos los 
 */
 
 function mostrarPokemon () {
-        //Código a ejecutar
-        
-        document.write(
-            '<ul>' +
-            '<li><img src="' + pokemones[0][0] + '.png" alt="Imagen Pikachu"></li>' +
-            '<li>Pokemon: ' + pokemones [0][0] + '</li>' +
-            '<li>Tipo: ' + pokemones [0][1] + '</li>' +
-            '<li>Poder: ' + pokemones [0][2] + '</li>' +
-            '</ul>'
+    //Código a ejecutar
+    if(pokemones.length == 0){
+            mensaje.innerHTML = 'Para ver tus pokemones primero agrega alguno!';
+    } else {
+ 
+//  Ciclo for --> Repetir Código
+        for(var r = 0 ; r < 3 ; r++){
+            // Código que se repite 
+            document.write(
+                '<ul>' +
+                '<li><img src="' + pokemones[r][0] + '.png" alt="Imagen Pikachu"></li>' +
+                '<li>Pokemon: ' + pokemones [r][0] + '</li>' +
+                '<li>Tipo: ' + pokemones [r][1] + '</li>' +
+                '<li>Poder: ' + pokemones [r][2] + '</li>' +
+                '</ul>'
             );
+        }; 
     }
+};
+
+
+
+    
+
+        
